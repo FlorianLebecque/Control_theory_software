e47af87a8d5772e730a6768b525ed5c4eded8ba7
smarbal <sebastien.martinez99@gmail.com>
2022-05-08 23:12:51 +0200

    Refactor lead-lag

--- package LAB.py
+++ package LAB.py
@@ -92,4 +92,38 @@ The function "PID_RT" appends new values to the vectors "MV", "MVP", "MVI", and 
                    
     MV.append(MV_TEMP)
         
-        
+
+def LeadLag_RT(MV,Kp,T_lead,T_lag,Ts,PV,PVInit=0,method='EBD'):
+    
+    """
+    The function "LeadLag_RT" needs to be included in a "for or while loop".
+    
+    :MV: input vector
+    :Kp: process gain
+    :T: lag time constant [s]
+    :Ts: sampling period [s]
+    :PV: output vector
+    :PVInit: (optional: default value is 0)
+    :method: discretisation method (optional: default value is 'EBD')
+        EBD: Euler Backward difference
+        EFD: Euler Forward difference
+        TRAP: Trapezoïdal method
+    
+    The function "LeadLag_RT" appends a value to the output vector "PV".
+    The appended value is obtained from a recurrent equation that depends on the discretisation method.
+    """    
+    if (T_lag != 0):
+        K = Ts/T_lag
+        if len(PV) == 0:
+            PV.append(PVInit)
+        else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
+            if method == 'EBD':
+                PV.append((1/(1+K)) * PV[-1] + ((K*Kp)/(1+K)) * ((1+(T_lead/Ts)) * MV[-1] - (T_lead/Ts) * MV[-2]))
+            elif method == 'EFD':
+                PV.append((1-K) * PV[-1] + K * Kp * ((T_lead/Ts) * MV[-1] + (1 - (T_lead/Ts)) * MV[-2]))
+            elif method == 'TRAP':
+                PV.append((1/(2 * T_lag+Ts)) * ((2 * T_lag-Ts) * PV[-1]+(2 * T_lead+Ts) * Kp * MV[-1] + (Ts-2*T_lead) * Kp * MV[-2]))
+            else:
+                PV.append((1/(1+K)) * PV[-1] + ((K*Kp)/(1+K)) * ((1+(T_lead/Ts)) * MV[-1] - (T_lead/Ts) * MV[-2]))
+    else:
+        PV.append(Kp*MV[-1])
